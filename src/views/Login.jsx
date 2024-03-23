1fefdea73335309b8cc5eadfa14dc4deb40cce72
Martin Calderón <elmcwl@gmail.com>
2024-03-22 21:45:25 -0700

    feat: added login form

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,5 +1,47 @@
 export default function Login() {
   return (
-    <div>Login</div>
+    <>
+      <h1 className="text-4xl font-black">Iniciar sesión</h1>
+      <p>Para crear un pedido debes iniciar sesión</p>
+      
+      <div className="bg-white shadow-md rounded-md mt-10 px-5 py-10">
+        <form>
+          
+          <div className="mb-4">
+            <label
+              className="text-slate-800"
+              htmlFor="email"
+            >Email</label>
+            <input
+              type="email"
+              id="email"
+              className="mt-2 w-full p-3 bg-gray-50"
+              name="email"
+              placeholder="Tu Email"
+            />
+          </div>
+          
+          <div className="mb-4">
+            <label
+              className="text-slate-800"
+              htmlFor="password"
+            >Contraseña</label>
+            <input
+              type="password"
+              id="password"
+              className="mt-2 w-full p-3 bg-gray-50"
+              name="password"
+              placeholder="Tu Contraseña"
+            />
+          </div>
+          
+          <input 
+            type="submit"
+            value='Iniciar Sesión'
+            className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
+          />
+        </form>
+      </div>
+    </>
   )
 }
