cc23c7417d08eb4ae462141682b7e3383d70b6f5
Martin Calderón <elmcwl@gmail.com>
2024-03-24 03:45:48 -0700

    feat: adding scroll and padding to main section in Layout component

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -9,7 +9,7 @@ export default function Layout() {
     <div className='md:flex'>
       <Sidebar />
       
-      <main className='flex-1'>
+      <main className='flex-1 h-screen overflow-y-scroll bg-gray-100 p-3'>
         <Outlet />
       </main>
       
