6c653b977aa83f2fbcd8b34a9c9af577d322383d
Martin Calderón <elmcwl@gmail.com>
2024-03-24 00:02:11 -0700

    feat: advances producto

---
+++ src/components/Producto.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Producto({ producto }) {
+  
+  const { nombre, precio, imagen, categoria_id } = producto
+  
+  return (
+    <div>
+      Producto
+    </div>
+  )
+}
--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -1,5 +1,20 @@
+import Producto from '../components/Producto.jsx'
+import { productos } from '../data/productos.js'
+
 export default function Inicio() {
   return (
-    <div>Inicio</div>
+    <>
+      <h1 className='text-4xl font-black'>Inicio</h1>
+      <p className='text-2xl my-10'>Elige y personaliza tu pedido</p>
+      
+      <div className='grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3'>
+        { productos.map(producto => (
+          <Producto 
+            key={producto.imagen}
+            producto={producto}
+          />
+        )) }
+      </div>
+    </>
   )
 }
