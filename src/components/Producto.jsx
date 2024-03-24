0daa714dd2607cf0a62e2cfa013cca88bb4735c5
Martin Calderón <elmcwl@gmail.com>
2024-03-24 03:37:00 -0700

    feat: added coin conversion in Producto component

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import {formatearDinero} from '../helpers'
 
 export default function Producto({ producto }) {
   
   const { nombre, precio, imagen } = producto
+  
   
   return (
     <div
@@ -16,8 +17,17 @@ export default function Producto({ producto }) {
       
       <div className='p-5'>
         <h3 className='text-2xl font-bold'>{ nombre }</h3>
-        <p className='mt-5 font-black text-4xl text-amber-400'>{ precio }</p>
+        <p className='mt-5 font-black text-4xl text-amber-400'>
+          { formatearDinero(precio) }
+        </p>
       </div>
+      
+      <button
+        type='button'
+        className='bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold'
+      >
+        Agregar
+      </button>
       
     </div>
   )
