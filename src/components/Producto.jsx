126008b58cf73e3fadd6eaa66ba9f8d8eb40016e
Martin Calderón <elmcwl@gmail.com>
2024-03-24 00:24:32 -0700

    feat: added .map in Producto component

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -2,11 +2,23 @@ import React from 'react'
 
 export default function Producto({ producto }) {
   
-  const { nombre, precio, imagen, categoria_id } = producto
+  const { nombre, precio, imagen } = producto
   
   return (
-    <div>
-      Producto
+    <div
+      className='border p-3 shadow bg-white'
+    >
+      <img
+        alt={ `imagen ${nombre}` }
+        className='w-full'
+        src={ `/img/${imagen}.jpg` }
+      />
+      
+      <div className='p-5'>
+        <h3 className='text-2xl font-bold'>{ nombre }</h3>
+        <p className='mt-5 font-black text-4xl text-amber-400'>{ precio }</p>
+      </div>
+      
     </div>
   )
 }
