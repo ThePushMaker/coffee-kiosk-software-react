191585cdee91fc6a4d90a9722b659644c08311b6
Martin Calderón <elmcwl@gmail.com>
2024-03-27 00:52:42 -0700

    feat: updating the modal when the user is editing their order

--- src/components/ModalProducto.jsx
+++ src/components/ModalProducto.jsx
@@ -71,7 +71,10 @@ export default function ModalProducto() {
           type="button"
           className="bg-indigo-600 hover:bg-indigo-800 px-5 py-2 mt-5 text-white
           font-bold uppercase rounded"
-          onClick={() => handleAgregarPedido({...producto, cantidad})}
+          onClick={() => {
+            handleAgregarPedido({...producto, cantidad})
+            handleClickModal()              
+          }}
         >
           Añadir al pedido
         </button>
