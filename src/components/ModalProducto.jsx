a088edcc1e70311c06ce465935afd2540eaa2cfa
Martin Calderón <elmcwl@gmail.com>
2024-03-26 22:46:30 -0700

    feat: Displaying the remaining product's information

--- src/components/ModalProducto.jsx
+++ src/components/ModalProducto.jsx
@@ -1,4 +1,5 @@
 import useQuiosco from "../hooks/useQuiosco"
+import {formatearDinero} from "../helpers"
 
 export default function ModalProducto() {
   const { producto, handleClickModal } = useQuiosco()
@@ -22,6 +23,23 @@ export default function ModalProducto() {
             </svg>
           </button>
         </div>
+        
+        <h1 className="text-3xl font-bold mt-5">
+          {producto?.nombre}
+        </h1>
+        
+        <p className="mt-5 font-black text-5xl text-amber-400">
+          {formatearDinero(producto?.precio)}
+        </p>
+        
+        <button
+          type="button"
+          className="bg-indigo-600 hover:bg-indigo-800 px-5 py-2 mt-5 text-white
+          font-bold uppercase rounded"
+        >
+          Añadir al pedido
+        </button>
+        
       </div>
     </div>
   )
