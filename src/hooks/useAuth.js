315924285c6c6549a3e512b51699b2774fdb9aa5
Martin Calderón <elmcwl@gmail.com>
2024-04-02 20:42:44 -0700

    feat: Revalidated the authenticated user with useSWR

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,10 +1,27 @@
+import useSWR from "swr"
 import clienteAxios from "../config/axios"
 export const useAuth = ({middleware, url}) => {
+  
+  const token = localStorage.getItem('AUTH_TOKEN')
+  
+  const {data: user, error, mutate} = useSWR('/user', () => 
+    clienteAxios('/user', {
+      headers: { 
+        Authorization: `Bearer ${token}`
+      }
+    })
+    .then(res => res.data)
+    .catch(error => {
+      throw Error(error?.response?.data?.errors)
+    })
+  )
+  
   const login = async (datos, setErrores) => {
     try {
       const {data} = await clienteAxios.post('/login', datos)
       localStorage.setItem('AUTH_TOKEN', data.token)
       setErrores([])
+      await mutate()
     } catch (error) {
       setErrores(Object.values(error.response.data.errors))
     }
@@ -15,6 +32,9 @@ export const useAuth = ({middleware, url}) => {
   const logout = () => {
     
   }
+  
+  console.log(user)
+  console.log(error)
   
   return {
     login,
