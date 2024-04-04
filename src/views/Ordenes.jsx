4d963c75baa5d122c150feaea34464a3032e2b53
Martin Calderón <elmcwl@gmail.com>
2024-04-03 20:34:40 -0700

    feat: displaying the content of the order

--- src/views/Ordenes.jsx
+++ src/views/Ordenes.jsx
@@ -11,14 +11,41 @@ export default function Ordenes() {
   
   const {data, error, isLoading} = useSWR('/pedidos', fetcher)
   
-  console.log(data?.data)
-  console.log(error)
-  console.log(isLoading)
+  if(isLoading) return 'Cargando...'
   
   return (
     <div>
       <h1 className='text-4xl font-black'>Ordenes</h1>
       <p className='text-2xl my-10'>Administra las ordenes desde aquí</p>
+      
+      <div>
+        {data.data.data.map(pedido => (
+          <div
+            key={pedido.id} 
+            className="p-5 bg-white shadow space-y-2 border-b"
+          >
+            <p className="text-xl font-bold text-slate-600">
+              Contenido del pedido:
+            </p>
+            
+            {pedido.productos.map(producto => (
+              <div 
+                key={producto.id}
+                className="border-b border-b-slate-200 last-of-type:border-none py-4"
+              >
+                <p className="text-sm">ID: {producto.id}</p>
+                <p className="text-sm">{producto.nombre}</p>
+                <p className="text-sm">
+                  Cantidad: {' '}
+                  <span className="font-bold">{producto.pivot.cantidad}</span>
+                </p>
+                
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
+    
   )
 }
